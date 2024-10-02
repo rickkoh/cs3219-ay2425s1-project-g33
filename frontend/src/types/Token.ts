3a7168bfc15d3b2abd034794071f2093d89080d5
Rick <rick.kohjiaxuan@gmail.com>
2024-10-02 16:57:03 +0800

    Update AccessTokenPayloadSchema and types

--- frontend/src/types/Token.ts
+++ frontend/src/types/Token.ts
@@ -1,19 +1,31 @@
 import { z } from "zod";
 import { createResponseSchema } from "./Response";
 
+export const AccessTokenSchema = z.string();
+export const RefreshTokenSchema = z.string();
+
 export const TokenPairSchema = z.object({
-  access_token: z.string(),
-  refresh_token: z.string(),
+  access_token: AccessTokenSchema,
+  refresh_token: RefreshTokenSchema,
 });
-export const AccessTokenSchema = TokenPairSchema.omit({
+
+export const AccessOnlySchema = TokenPairSchema.omit({
   refresh_token: true,
 });
 
 export const TokenPairResponseSchema = createResponseSchema(TokenPairSchema);
-export const AccessTokenResponseSchema =
-  createResponseSchema(AccessTokenSchema);
+export const AccessTokenResponseSchema = createResponseSchema(AccessOnlySchema);
 
-export type TokenPair = z.infer<typeof TokenPairSchema>;
+export const AccessTokenPayloadSchema = z.object({
+  sub: z.string(),
+  email: z.string().email(),
+  iat: z.number(),
+  exp: z.number(),
+});
+
 export type AccessToken = z.infer<typeof AccessTokenSchema>;
+export type TokenPair = z.infer<typeof TokenPairSchema>;
 export type TokenPairResponse = z.infer<typeof TokenPairResponseSchema>;
 export type AccessTokenResponse = z.infer<typeof AccessTokenResponseSchema>;
+
+export type AccessTokenPayload = z.infer<typeof AccessTokenPayloadSchema>;
