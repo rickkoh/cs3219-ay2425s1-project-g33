45e078e79600ce25debb6f1264a8e58ece34921b
jmsandiegoo <jmsandiego0710@gmail.com>
2024-10-06 12:38:17 +0800

    Update user type to support onboarding as well

--- frontend/src/types/User.ts
+++ frontend/src/types/User.ts
@@ -1,12 +1,13 @@
 import { z } from "zod";
 import { ProficiencyEnum } from "./Proficiency";
 import { LanguageEnum } from "./Languages";
+import { RoleEnum } from "./Role";
 
 const UserProfileSchema = z.object({
   username: z.string(),
   displayName: z.string(),
   email: z.string().email(),
-  roles: z.string().array(),
+  roles: z.array(RoleEnum),
   proficiency: ProficiencyEnum,
   languages: z.array(LanguageEnum),
 });
