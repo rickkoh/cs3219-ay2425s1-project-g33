b77bc1a50d291757b158e54651572c15a9553b69
Rick <rick.kohjiaxuan@gmail.com>
2024-10-09 21:06:53 +0800

    Update import path for FindMatchButton component

--- frontend/src/app/dashboard/_components/SideContents.tsx
+++ frontend/src/app/dashboard/_components/SideContents.tsx
@@ -1,17 +1,12 @@
 import { ProfileMiniDetailsCard } from "@/components/ProfileMiniDetailsCard";
 import StreakCalendarCard from "./StreakCalendarCard";
 import { QuestionsStatsCard } from "./QuestionsStatsCard";
-import { FindMatchButton } from "./FindMatchButton";
+import FindMatchButton from "./FindMatch/FindMatchButton";
 import { cn } from "@/lib/utils";
 
 export default function SideContents() {
   return (
-    <div
-      className={cn(
-        "relative h-full w-full",
-        "col-span-4 xl:col-span-3"
-      )}
-    >
+    <div className={cn("relative h-full w-full", "col-span-4 xl:col-span-3")}>
       <aside className="sticky flex flex-col gap-4 h-fit top-[5.5rem]">
         <ProfileMiniDetailsCard />
         <StreakCalendarCard />
