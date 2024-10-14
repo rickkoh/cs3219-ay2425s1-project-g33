6684fe8a172f548ba18314a70dd9388da24306e7
Rick <rick.kohjiaxuan@gmail.com>
2024-10-14 21:32:44 +0800

    Refactor withAuthMiddleware to handle protected routes

--- frontend/src/middlewares/withAuthMiddleware.ts
+++ frontend/src/middlewares/withAuthMiddleware.ts
@@ -1,54 +1,29 @@
-import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextFetchEvent, NextResponse } from "next/server";
 import { CustomMiddleware } from "./chain";
-import { createResponseRedirect, setAuthCookieSession } from "@/lib/utils";
 
-export function withAuthMiddlware(
-  middleware: CustomMiddleware
-): CustomMiddleware {
+const protectedRoutes = ["/onboard", "/dashboard", "/profile"];
+
+export function withAuthMiddleware(next: CustomMiddleware): CustomMiddleware {
   return async (
     request: NextRequest,
     event: NextFetchEvent,
     response: NextResponse
   ) => {
-    const accessToken = request.cookies.get("access_token")?.value;
-
-    if (request.nextUrl.pathname.startsWith("/auth")) {
-      if (accessToken) {
-        const previousPage = request.headers.get("referer");
-        const newResponse = createResponseRedirect(
-          new URL(previousPage || "/dashboard", request.url)
-        );
-        return newResponse;
-      }
-    } else if (request.nextUrl.pathname.startsWith("/oauth")) {
-      console.log("oauth middleware for oauth invoked");
-      const accessToken = request.nextUrl.searchParams.get("accessToken");
-      const refreshToken = request.nextUrl.searchParams.get("refreshToken");
-
-      if (!accessToken || !refreshToken) {
-        const newResponse = createResponseRedirect(
-          new URL("/auth/signin", request.url)
-        );
-        return newResponse;
-      }
-
-      const newResponse = createResponseRedirect(
-        new URL("/dashboard", request.url)
-      );
-      setAuthCookieSession(accessToken, refreshToken, newResponse);
-      return newResponse;
-    } else {
-      // for required auth routes
-      if (!accessToken) {
-        const newResponse = createResponseRedirect(
-          new URL("/auth/signin", request.url)
-        );
-        return newResponse;
-      }
-
-      // TODO refresh token if expired call nestjs backend
+    if (
+      !protectedRoutes.some((route) =>
+        request.nextUrl.pathname.startsWith(route)
+      )
+    ) {
+      return next(request, event, response);
     }
 
-    return middleware(request, event, response);
+    const accessToken = request.cookies.get("access_token")?.value;
+    const refreshToken = request.cookies.get("refreshToken")?.value;
+
+    if (!accessToken || refreshToken) {
+      return NextResponse.redirect(new URL("/auth/signin", request.url));
+    }
+
+    return next(request, event, response);
   };
 }
