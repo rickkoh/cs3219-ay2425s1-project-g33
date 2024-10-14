33b7ef0d063c6fc4a91b82bafb942f39446dde41
Rick <rick.kohjiaxuan@gmail.com>
2024-10-14 21:35:19 +0800

    Update AuthLayout to redirect to /dashboard if user is logged in

--- frontend/src/app/auth/layout.tsx
+++ frontend/src/app/auth/layout.tsx
@@ -2,7 +2,20 @@ import Navbar from "@/components/Navbar";
 import { Toaster } from "@/components/ui/toaster";
 import { ReactNode } from "react";
 
-export default function AuthLayout({ children }: { children: ReactNode }) {
+import { getCurrentUser } from "@/services/userService";
+import { redirect } from "next/navigation";
+
+export default async function AuthLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const currentUser = await getCurrentUser();
+
+  if (currentUser.statusCode === 200 && currentUser.data) {
+    redirect("/dashboard");
+  }
+
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar isMinimal={true} className="relative mt-8 border-b-0" />
