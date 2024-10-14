c6178461e0ad2fe91f47486d7afecedbf74640f9
jmsandiegoo <jmsandiego0710@gmail.com>
2024-10-15 01:59:45 +0800

    Fix dashboard redirection to onboard is not onboarded

--- frontend/src/app/dashboard/layout.tsx
+++ frontend/src/app/dashboard/layout.tsx
@@ -1,9 +1,20 @@
 import Navbar from "@/components/Navbar";
 import { Toaster } from "@/components/ui/toaster";
+import { getCurrentUser } from "@/services/userService";
+import { redirect } from "next/navigation";
 import React from "react";
 import { PropsWithChildren } from "react";
 
 export default async function DashboardLayout({ children }: PropsWithChildren) {
+  const currentUser = await getCurrentUser();
+
+  if (currentUser.statusCode === 401 || !currentUser.data) {
+    redirect("/auth/signin");
+  }
+
+  if (!currentUser.data?.isOnboarded) {
+    redirect("/onboard");
+  }
 
   return (
     <>
