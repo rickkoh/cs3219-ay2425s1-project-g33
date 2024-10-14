bbb2049e9326cd44f654efeffde5f0e15829e1d0
Rick <rick.kohjiaxuan@gmail.com>
2024-10-14 17:21:53 +0800

    Refactor chain.ts to activate middleware in sequence

--- frontend/src/middlewares/chain.ts
+++ frontend/src/middlewares/chain.ts
@@ -1,33 +1,25 @@
-// Utility to chain middleware functions together
-
 import { NextMiddlewareResult } from "next/dist/server/web/types";
-import { NextResponse } from "next/server";
-import type { NextFetchEvent, NextRequest } from "next/server";
+import { NextRequest, NextFetchEvent, NextResponse } from "next/server";
 
 export type CustomMiddleware = (
   request: NextRequest,
   event: NextFetchEvent,
   response: NextResponse
-) => NextMiddlewareResult | Promise<NextMiddlewareResult>;
+) => Promise<NextMiddlewareResult> | NextMiddlewareResult;
 
-type MiddlewareFactory = (middleware: CustomMiddleware) => CustomMiddleware;
+type MiddlewareFactory = (next: CustomMiddleware) => CustomMiddleware; // Middleware factory that chains the next middleware
 
 export function chain(
-  functions: MiddlewareFactory[],
-  index = 0
+  middlewareFactories: MiddlewareFactory[]
 ): CustomMiddleware {
-  const current = functions[index];
+  // Base case when no middleware is left in the chain
+  const next: CustomMiddleware = async (request, event, response) => response;
 
-  if (current) {
-    const next = chain(functions, index + 1);
-    return current(next);
-  }
-
-  return (
-    request: NextRequest,
-    event: NextFetchEvent,
-    response: NextResponse
-  ) => {
-    return response;
-  };
+  // Start chaining middlewares from the last one to the first one
+  // First middleware will be on top of the stack and will be called first
+  return middlewareFactories
+    .reverse()
+    .reduce((nextMiddleware, currentFactory) => {
+      return currentFactory(nextMiddleware);
+    }, next);
 }
