d73881cef3cc6db3c5553de2c60b141ebc04cd65
jmsandiegoo <jmsandiego0710@gmail.com>
2024-10-16 00:43:28 +0800

    Fix route handler code format

--- frontend/src/app/oauth/route.ts
+++ frontend/src/app/oauth/route.ts
@@ -3,22 +3,22 @@ import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
-    const queryParams = request.nextUrl.searchParams;
-    const accessToken = queryParams.get("accessToken");
-    const refreshToken = queryParams.get("refreshToken");
+  const queryParams = request.nextUrl.searchParams;
+  const accessToken = queryParams.get("accessToken");
+  const refreshToken = queryParams.get("refreshToken");
 
-    if (!accessToken || !refreshToken) {
-      return NextResponse.redirect(new URL("/auth/signin", request.url));
-    }
+  if (!accessToken || !refreshToken) {
+    return NextResponse.redirect(new URL("/auth/signin", request.url));
+  }
 
-    const tokenPair: TokenPair = TokenPairSchema.parse({
-      access_token: accessToken,
-      refresh_token: refreshToken,
-    });
+  const tokenPair: TokenPair = TokenPairSchema.parse({
+    access_token: accessToken,
+    refresh_token: refreshToken,
+  });
 
-    const cookieStore = cookies();
-    cookieStore.set("access_token", tokenPair.access_token);
-    cookieStore.set("refresh_token", tokenPair.refresh_token);
+  const cookieStore = cookies();
+  cookieStore.set("access_token", tokenPair.access_token);
+  cookieStore.set("refresh_token", tokenPair.refresh_token);
 
-    return NextResponse.redirect(new URL("/dashboard", request.url));
+  return NextResponse.redirect(new URL("/dashboard", request.url));
 }
