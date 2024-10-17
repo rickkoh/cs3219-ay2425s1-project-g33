5e0937d3176b486854e0c9608a3e538302d6c436
Rick <rick.kohjiaxuan@gmail.com>
2024-10-17 14:58:45 +0800

    Add function to get initials from name string

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -39,3 +39,17 @@ export function createResponseRedirect(
 
   return redirectResponse;
 }
+
+export function getInitialsFromName(name: string) {
+  // defined here for encapsulation
+  const chunks = name.split(",");
+
+  if (chunks.length > 1) {
+    const initials: string[] = [];
+    chunks.forEach((chunk) => initials.push(chunk[0]));
+    return initials.join("");
+  } else {
+    const [fname, lname] = chunks[0].split(" ");
+    return `${fname[0]}${lname ? lname[0] : ""}`;
+  }
+}
