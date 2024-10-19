1299d4111c12f5f77b13df09e956737d37e0a34d
Rick <rick.kohjiaxuan@gmail.com>
2024-10-19 13:50:08 +0800

    Update ForgotPasswordForm component and form validation

--- frontend/src/app/forgotpassword/sendmail/_components/ForgotPasswordForm.tsx
+++ frontend/src/app/forgotpassword/sendmail/_components/ForgotPasswordForm.tsx
@@ -10,7 +10,7 @@ import {
 } from "@/components/ui/card";
 import Link from "next/link";
 
-import { useCallback, useState } from "react";
+import { useState } from "react";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { resetPassword } from "@/services/authService";
@@ -23,104 +23,104 @@ import { Mail } from "lucide-react";
 import { useToast } from "@/hooks/use-toast";
 
 const FormSchema = z.object({
-    email: z.string().email("Please enter a valid email."),
-  });
+  email: z.string().email("Please enter a valid email."),
+});
 
 export default function ForgotPasswordForm() {
-    const router = useRouter();
-    const { toast } = useToast();
-    const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
-  
-    const methods = useForm<z.infer<typeof FormSchema>>({
-      resolver: zodResolver(FormSchema),
-    });
-  
-    const { handleSubmit, formState } = methods;
-  
-    const onSubmit = async (data: z.infer<typeof FormSchema>) => {
-      try {
-        const response = await resetPassword(data.email); 
+  const router = useRouter();
+  const { toast } = useToast();
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
 
-        if (response.statusCode === 200 || response.statusCode === 201) {
-          toast({
-            title: "Success!",
-            description: "A link has been sent to your email to reset your password."});
-          router.push('/forgotpassword/sendmail');
-        } else {
-          toast({
-            title: 'Error!',
-            description: response.message,
-          });
-        }
+  const methods = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+  });
 
-      } catch (error) {
-        console.error('Error during reset password:', error);
+  const { handleSubmit, formState } = methods;
 
+  const onSubmit = async (data: z.infer<typeof FormSchema>) => {
+    try {
+      const response = await resetPassword(data.email);
+
+      if (response.statusCode === 200 || response.statusCode === 201) {
         toast({
-          title: 'Error!',
-          description: 'An error occurred. Please try again.',
+          title: "Success!",
+          description:
+            "A link has been sent to your email to reset your password.",
         });
-      } finally {
-        setIsSubmitting(false);
+        router.push("/forgotpassword/sendmail");
+      } else {
+        toast({
+          title: "Error!",
+          description: response.message,
+        });
       }
-    };
-  
-    return (
-      <Card className="p-2 mt-3">
-        <CardHeader>
-          <CardTitle className="text-xl">Forgot Password?</CardTitle>
-          <CardDescription className="text-card-foreground-100">
-            No worries we’ll send you reset instructions.
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <div className="flex flex-col">
-            {/* To input log in details */}
-            <Form {...methods}>
-              <form onSubmit={handleSubmit(onSubmit)}>
-                <div className="flex flex-col gap-y-4">
-                  {/* Email Field */}
-                  <TextInput
-                    label={""}
-                    name="email"
-                    type="email"
-                    placeholder="Email"
-                    Icon={Mail}
-                    className="bg-input-background-100"
-                  />
-                </div>
-  
-                {/* Reset Password Button */}
-                <Button
-                  type="submit"
-                  disabled={formState.isSubmitting || isSubmitting}
-                  className="w-full py-2 mt-5 rounded-md bg-primary"
-                >
-                  {formState.isSubmitting || isSubmitting ? (
-                    <LoadingSpinner />
-                  ) : (
-                    "Reset Password"
-                  )}
-                </Button>
-              </form>
-            </Form>
-  
-            {/* Sign in here */}
-            <div className="justify-center mt-6 text-sm text-center">
-              <p>
-                {" "}
-                Back to{" "}
-                <Link
-                  href="/auth/signin"
-                  className="hover:underline text-primary"
-                >
-                  Sign in
-                </Link>{" "}
-              </p>
-            </div>
+    } catch (error) {
+      console.error("Error during reset password:", error);
+
+      toast({
+        title: "Error!",
+        description: "An error occurred. Please try again.",
+      });
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  return (
+    <Card className="p-2 mt-3">
+      <CardHeader>
+        <CardTitle className="text-xl">Forgot Password?</CardTitle>
+        <CardDescription className="text-card-foreground-100">
+          No worries we’ll send you reset instructions.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="flex flex-col">
+          {/* To input log in details */}
+          <Form {...methods}>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="flex flex-col gap-y-4">
+                {/* Email Field */}
+                <TextInput
+                  label={""}
+                  name="email"
+                  type="email"
+                  placeholder="Email"
+                  Icon={Mail}
+                  className="bg-input-background-100"
+                />
+              </div>
+
+              {/* Reset Password Button */}
+              <Button
+                type="submit"
+                disabled={formState.isSubmitting || isSubmitting}
+                className="w-full py-2 mt-5 rounded-md bg-primary"
+              >
+                {formState.isSubmitting || isSubmitting ? (
+                  <LoadingSpinner />
+                ) : (
+                  "Reset Password"
+                )}
+              </Button>
+            </form>
+          </Form>
+
+          {/* Sign in here */}
+          <div className="justify-center mt-6 text-sm text-center">
+            <p>
+              {" "}
+              Back to{" "}
+              <Link
+                href="/auth/signin"
+                className="hover:underline text-primary"
+              >
+                Sign in
+              </Link>{" "}
+            </p>
           </div>
-        </CardContent>
-      </Card>
-    );
-  }
-  
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
