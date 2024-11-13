adc452f2c0ffd153184ad29ad63f30854c84c673
Evan Yan <103996156+evanyan13@users.noreply.github.com>
2024-11-13 10:00:27 +0800

    Fix connection between FE and Yjs (#133)
    * Install ESLint for Github Action
    
    * Fix issue connecting to yjs from fe

--- frontend/src/app/collaboration/_components/Editor/CollaborativeWhiteboard/CollaborativeWhiteboardTab.tsx
+++ frontend/src/app/collaboration/_components/Editor/CollaborativeWhiteboard/CollaborativeWhiteboardTab.tsx
@@ -2,9 +2,9 @@ import CollaborativeWhiteboard from "./CollaborativeWhiteboard";
 import { useSessionContext } from "@/contexts/SessionContext";
 
 export default function CollaborativeEditorTab() {
-  const {userProfile, sessionId} = useSessionContext()
+  const { userProfile, sessionId } = useSessionContext()
 
-  const socketUrl = process.env.PUBLIC_Y_WEBSOCKET_URL || "ws://localhost:4001";
+  const socketUrl = process.env.NEXT_PUBLIC_Y_WEBSOCKET_URL || "ws://localhost:4001";
 
   if (!userProfile) {
     return <div>Loading user profile...</div>;
